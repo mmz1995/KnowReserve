1ba895e6b625118a4868cc20dadae7275880f593
MaMengzhen <1017373019@qq.com>
2018-05-22 13:14:12 +0800

    add ExceptionExample.cs 异常

---
+++ ExceptionTest/ExceptionTest/ExceptionExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTest
+{
+    public class Sentence
+    {
+        public Sentence(string s)
+        {
+            Value = s;    
+        }
+        public string Value { get; set; }
+        public char GetFirstChar()
+        {
+            try//疑似异常语句块
+            {
+                return Value[0];
+            }
+            catch (IndexOutOfRangeException e)//异常处理语句----- 异常处理分类，越精细越可能造成异常的类别，位置顺序越靠上
+            {
+                Console.WriteLine($"Exception:{e.Message}");
+                throw;
+            }
+        }
+    }
+
+    class ExceptionExample
+    {
+        static void Main(string[] args)
+        {
+            Sentence s = new Sentence(null);//未实例化，将抛出异常
+            //Sentence s = new Sentence("zhen");//正常调用
+            Console.WriteLine($"the first character is {s.GetFirstChar()}");
+        }
+    }
+}
